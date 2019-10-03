8647763bf8a6bc529ee3f0953f1f472f026e631a
Philippe Laflamme <philippe.laflamme@gmail.com>
2019-10-02 20:36:54 -0400

    Adds work queues.

--- src/puzzle7/mod.rs
+++ src/puzzle7/mod.rs
@@ -36,6 +36,12 @@ struct Solution {
     time: u32
 }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+struct Work {
+    finish_at: u32, // This needs to come first for ordering
+    step: char,
+}
+
 struct Puzzle7 {
     deps: Vec<Dependency>
 }
@@ -52,29 +58,77 @@ impl Puzzle7 {
         steps.values().cloned().collect()
     }
 
-    fn solve(&self, workers: u8, cost: u32) -> Solution {
-        let mut steps = self.to_steps();
-        let mut steps_ran = HashSet::new();
-        let mut run_sequence: Vec<char> = Vec::new();
+    fn cost(base_cost: u32, step: &char) -> u32 {
+        *step as u32 - 'A' as u32 + 1 + base_cost
+    }
 
-        while !steps.is_empty() {
-            steps.iter().for_each(|x|println!("{:?}", x));
-            let mut new_steps_to_run = steps.iter()
-                .filter_map(|x| if x.depends_on.is_empty() { Some(Reverse(x.id)) } else { None } )
-                .collect::<BinaryHeap<_>>();
-            new_steps_to_run.pop().iter().for_each(|step| {
-                steps_ran.insert(step.0);
-                run_sequence.push(step.0);
+    fn solve(&self, workers: u8, base_cost: u32) -> Solution {
+        let mut steps = self.to_steps();
+        let mut steps_ran: HashSet<char> = HashSet::new();
+        let mut run_sequence: Vec<char> = Vec::new();
+        let mut work_queue: BinaryHeap<Reverse<Work>> = BinaryHeap::new();
+        let mut current_time: u32 = 0;
+
+        // TODO: this is pretty disgusting, but here's what's going on
+        //   we first check if anything in the work queue is done (items that have finish_at == current_time)
+        //     anything done is added to steps_ran and run_sequence
+        //   we prune the list of dependencies for the pending steps
+        //   we check for new work (steps that have no dependencies pending)
+        //     fill in the work queue to capacity with available work, marking when each work will finish
+        //   prune the list of pending steps (remove then ones that are running)
+        //   advance the time to when the top item of the work queue will finish
+        //   repeat until there are no outstanding steps left and there are no items in the work queue
+        while !steps.is_empty() || !work_queue.is_empty() {
+            // check for finished work
+            fn clear_finished_work(q: &mut BinaryHeap<Reverse<Work>>, now: u32, finished: &mut Vec<Work>) {
+                match q.peek() {
+                    None => (),
+                    Some(peeked) =>
+                        if peeked.0.finish_at == now {
+                            finished.push(peeked.0);
+                            q.pop();
+                            clear_finished_work(q, now, finished);
+                        }
+                }
+            }
+            let mut finished = Vec::new();
+            clear_finished_work(&mut work_queue, current_time, &mut finished);
+            finished.iter().for_each(|work| {
+                run_sequence.push(work.step);
+                steps_ran.insert(work.step);
             });
-            steps.retain(|x| !steps_ran.contains(&x.id));
+
             steps.iter_mut()
                 .for_each(|x| {
                     x.depends_on.retain(|x| !steps_ran.contains(x) )
                 });
+
+            // find some new work
+            let mut new_steps_to_run = steps.iter()
+                .filter_map(|x| if x.depends_on.is_empty() { Some(Reverse(x.id)) } else { None } )
+                .collect::<BinaryHeap<_>>();
+
+            while work_queue.len() < workers as usize && !new_steps_to_run.is_empty() {
+                let work = new_steps_to_run.pop().unwrap();
+                let finish_at = current_time + Puzzle7::cost(base_cost, &work.0);
+                work_queue.push(Reverse(Work { step: work.0, finish_at }));
+            }
+
+            // remove steps under work
+            steps.retain(|step| work_queue.iter().find(|work| work.0.step == step.id).is_none());
+
+            println!("{}", current_time);
+            steps.iter().for_each(|x| println!("{:?}", x));
+            work_queue.iter().for_each(|x| println!("{:?}", x));
             println!("{:?}", run_sequence);
+
+            match work_queue.peek() {
+                None => (),
+                Some(Reverse(w)) => current_time = w.finish_at
+            }
         }
 
-        Solution { sequence: run_sequence, time: 0 }
+        Solution { sequence: run_sequence, time: current_time }
     }
 }
 
@@ -113,7 +167,7 @@ mod test {
     #[test]
     fn test_part2() {
         let solution = Puzzle7 { deps: example_input() }.solve(2, 0);
-        assert_eq!(solution.sequence.iter().collect::<String>(), "CABDFE");
+        assert_eq!(solution.sequence.iter().collect::<String>(), "CABFDE");
         assert_eq!(solution.time, 15);
     }
 }
