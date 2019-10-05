89aea2354bd976af6b04ee3f35eb60a5df01afde
Philippe Laflamme <philippe.laflamme@gmail.com>
2019-10-04 21:24:53 -0400

    Puzzle 8 part 1

--- src/puzzle8/mod.rs
+++ src/puzzle8/mod.rs
@@ -11,9 +11,61 @@ struct Puzzle8 {
     nodes: Vec<u32>
 }
 
+struct Node {
+    metadata: Vec<u32>,
+    children: Vec<Node>
+}
+
+impl Node {
+
+    fn new(flat: &mut Vec<u32>) -> Node {
+        flat.reverse();
+        Node::mk_tree(flat)
+    }
+
+    fn mk_tree(flat: &mut Vec<u32>) -> Node {
+        let n_children = flat.pop().expect("missing number of children");
+        let n_meta = flat.pop().expect("missing number of metadata");
+
+        let mut children = Vec::new();
+        for _ in 0..n_children {
+            children.push(Node::mk_tree(flat));
+        }
+        let mut metadata = Vec::new();
+        for _ in 0..n_meta {
+            let v = flat.pop().expect("missing metadata");
+            metadata.push(v);
+        }
+
+        Node { metadata, children }
+    }
+
+    fn iter(&self) -> NodeIterator {
+        let a = std::iter::once(self);
+        let more = self.children.iter().flat_map(|x| x.iter());
+        NodeIterator { iter: Box::new(a.chain(more)) }
+    }
+}
+
+// https://amos.me/blog/2019/recursive-iterators-rust/
+struct NodeIterator<'a> {
+    iter: Box<dyn Iterator<Item = &'a Node> + 'a>
+}
+
+impl<'a> Iterator for NodeIterator<'a> {
+    type Item = &'a Node;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+}
+
 impl crate::Puzzle for Puzzle8 {
     fn part1(&self) -> String {
-        unimplemented!()
+        let root = Node::new(&mut self.nodes.clone());
+
+        let sum: u32 = root.iter().flat_map(|x| x.metadata.iter()).sum();
+        sum.to_string()
     }
 
     fn part2(&self) -> String {
@@ -36,5 +88,5 @@ mod test {
     }
 
     #[test]
-    fn test_part2() {}
+    fn test_part2() { unimplemented!() }
 }
