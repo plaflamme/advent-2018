8abec61c4f5aa5a8e784f6dc7ef76c8f279d4078
Philippe Laflamme <philippe.laflamme@gmail.com>
2019-10-13 22:04:40 -0400

    Puzzle 11 part 2, but it's very slow.
    There's something to do with the fact that power goes from -5 to 4, but
    I can't figure it out at the moment.

--- src/puzzle11/mod.rs
+++ src/puzzle11/mod.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use std::collections::HashMap;
+use std::fmt::{Display, Formatter, Error};
 
 fn power_level(x: u16, y: u16, serial_number: u16) -> i32 {
     let rack_id = x as i32 + 10;
@@ -32,31 +33,60 @@ impl Grid {
         Grid { fuel_cells }
     }
 
-    fn tile(&self, pt: &Pt) -> Vec<i32> {
-        let mut cells = Vec::with_capacity(9);
-        for y in pt.y..(pt.y + 3) {
-            for x in pt.x..(pt.x + 3) {
-                cells.push(*self.fuel_cells.get(&Pt{x,y} ).expect("invalid cell coordinate"))
+    fn tile_power(&self, pt: &Pt, side: u16) -> i32 {
+        let mut power = 0;
+        for y in pt.y..(pt.y + side) {
+            for x in pt.x..(pt.x + side) {
+                power += *self.fuel_cells.get(&Pt{x,y} ).expect("invalid cell coordinate");
             }
         }
-        cells
+        power
     }
 
-    fn iter(&self) -> Tile {
-        Tile { next: Some(Pt { x: 1, y: 1 }) }
+    fn iter(&self, side: u16) -> Tile {
+        Tile { side, next: Some(Pt { x: 1, y: 1 }) }
     }
 
-    fn solve(&self) -> Pt {
-        self.iter()
+    fn solve(&self, side: u16) -> Pt {
+        self.iter(side)
             .max_by_key(|x| {
-                let total_power: i32 = self.tile(x).iter().sum();
-                total_power
+                self.tile_power(x, side)
             })
             .expect("empty grid")
+    }
+
+    fn solve_all(&self) -> (Pt, u16) {
+        let mut max_power = -1000000;
+        let mut winning_pt: Pt = Pt {x:1,y:1};
+        let mut winning_side = 1;
+        for side in (1..=300) {
+            let pt = self.solve(side);
+            let power = self.tile_power(&pt, side);
+            if power > max_power {
+                max_power = power;
+                winning_pt = pt;
+                winning_side = side;
+                println!("{:?} {}, {}", winning_pt, side, max_power);
+            }
+        }
+        (winning_pt, winning_side)
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        for y in 1..50 {
+            for x in 1..50 {
+                write!(f, "{:2} ", self.fuel_cells.get(&Pt{x,y}).expect("fudge"))?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
 }
 
 struct Tile {
+    side: u16,
     next: Option<Pt>
 }
 
@@ -66,8 +96,8 @@ impl Iterator for Tile {
     fn next(&mut self) -> Option<Self::Item> {
         let next = self.next.clone();
         self.next = match self.next {
-            Some(Pt{x,y}) if x + 4 <= 300 => Some(Pt { x: x + 1, y }),
-            Some(Pt{x:_,y}) if y + 4 <= 300 => Some(Pt { x: 1, y: y + 1 }),
+            Some(Pt{x,y}) if x + self.side + 1 <= 300 => Some(Pt { x: x + 1, y }),
+            Some(Pt{x:_,y}) if y + self.side + 1 <= 300 => Some(Pt { x: 1, y: y + 1 }),
             _ => None
         };
         next
@@ -85,11 +115,13 @@ struct Puzzle11 {
 impl crate::Puzzle for Puzzle11 {
     fn part1(&self) -> String {
         let grid = Grid::new(self.serial_number);
-        format!("{:?}", grid.solve())
+        format!("{:?}", grid.solve(3))
     }
 
     fn part2(&self) -> String {
-        unimplemented!()
+        let grid = Grid::new(self.serial_number);
+        let (pt, side) = grid.solve_all();
+        format!("{},{},{}", pt.x, pt.y, side)
     }
 }
 
@@ -107,23 +139,38 @@ mod test {
     #[test]
     fn iterator() {
         let grid = Grid::new(18);
-        assert_eq!(Some(Pt{x:1,y:1}), grid.iter().next());
+        assert_eq!(Some(Pt{x:1,y:1}), grid.iter(3).next());
 
-        let tiles = grid.iter().collect::<Vec<_>>();
+        let tiles = grid.iter(3).collect::<Vec<_>>();
         assert_eq!(297*297, tiles.len());
     }
 
     #[test]
-    fn grid() {
+    fn part1() {
         let grid = Grid::new(18);
-        let max_tile = grid.solve();
+        let max_tile = grid.solve(3);
         assert_eq!(Pt{x:33,y:45}, max_tile);
-        assert_eq!(29, grid.tile(&Pt{x:33,y:45}).iter().sum());
+        assert_eq!(29, grid.tile_power(&max_tile, 3));
 
         let grid = Grid::new(42);
-        let max_tile = grid.solve();
+        let max_tile = grid.solve(3);
         assert_eq!(Pt{x:21,y:61}, max_tile);
-        assert_eq!(30, grid.tile(&Pt{x:21,y:61}).iter().sum());
+        assert_eq!(30, grid.tile_power(&max_tile, 3));
+    }
+
+    #[test]
+    fn part2() {
+        let grid = Grid::new(18);
+        println!("{}", grid);
+        let (max_tile, side) = grid.solve_all();
+        assert_eq!(16, side);
+        assert_eq!(Pt{x:90,y:269}, max_tile);
+        assert_eq!(29, grid.tile_power(&max_tile, side));
+
+        let grid = Grid::new(42);
+        let (max_tile, side) = grid.solve_all();
+        assert_eq!(Pt{x:21,y:61}, max_tile);
+        assert_eq!(30, grid.tile_power(&max_tile, side));
     }
 
 }
