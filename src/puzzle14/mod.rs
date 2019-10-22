ba498ea8f67493fb2dc5fe0c384f0c8030a6f8fa
Philippe Laflamme <philippe.laflamme@gmail.com>
2019-10-21 21:15:09 -0400

    Use VecDeque

--- src/puzzle14/mod.rs
+++ src/puzzle14/mod.rs
@@ -1,12 +1,21 @@
 use std::str::FromStr;
+use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Scoreboard {
-    recipe_scores: Vec<u8>,
+    recipe_scores: VecDeque<u8>,
     current_recipes: (usize, usize),
 }
 
 impl Scoreboard {
+
+    fn new() -> Self {
+        Scoreboard::from(vec![3,7], (0,1))
+    }
+
+    fn from(recipe_scores: Vec<u8>, current_recipes: (usize, usize)) -> Self {
+        Scoreboard { recipe_scores: VecDeque::from(recipe_scores), current_recipes }
+    }
 
     fn score_of(&self, recipe: usize) -> u8 {
         *self.recipe_scores.get(recipe).expect(&format!("missing recipe at {}", recipe))
@@ -18,9 +27,9 @@ impl Scoreboard {
 
         let new_score = a + b;
         if new_score >= 10 {
-            self.recipe_scores.push((new_score / 10) % 10)
+            self.recipe_scores.push_back((new_score / 10) % 10)
         }
-        self.recipe_scores.push(new_score % 10);
+        self.recipe_scores.push_back(new_score % 10);
 
         self.current_recipes.0 = (self.current_recipes.0 + a as usize + 1) % self.recipe_scores.len();
         self.current_recipes.1 = (self.current_recipes.1 + b as usize + 1) % self.recipe_scores.len();
@@ -37,7 +46,11 @@ impl Scoreboard {
             .take(10)
             .map(|score| score.to_string())
             .collect::<String>()
+    }
 
+    fn solve_part_2(&mut self, pattern: Vec<u8>) -> usize {
+
+        unimplemented!()
     }
 }
 
@@ -51,7 +64,7 @@ struct Puzzle14 {
 
 impl crate::Puzzle for Puzzle14 {
     fn part1(&self) -> String {
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard = Scoreboard::new();
         scoreboard.solve_after_recipes(self.seed)
     }
 
@@ -66,37 +79,37 @@ mod test {
 
     #[test]
     fn scoreboard() {
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard =Scoreboard::new();
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0], current_recipes: (0,1) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0], (0,1)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0], current_recipes: (4,3) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0], (4,3)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1], current_recipes: (6,4) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1], (6,4)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1,2], current_recipes: (0,6) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1,2], (0,6)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1,2,4], current_recipes: (4,8) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1,2,4], (4,8)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1,2,4,5], current_recipes: (6,3) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1,2,4,5], (6,3)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1,2,4,5,1], current_recipes: (8,4) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1,2,4,5,1], (8,4)), scoreboard);
         scoreboard.step();
-        assert_eq!(Scoreboard { recipe_scores: vec![3,7,1,0,1,0,1,2,4,5,1,5], current_recipes: (1,6) }, scoreboard);
+        assert_eq!(Scoreboard::from(vec![3,7,1,0,1,0,1,2,4,5,1,5], (1,6)), scoreboard);
     }
 
     #[test]
     fn part1() {
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard = Scoreboard::from(vec![3,7], (0,1));
         assert_eq!("5158916779", scoreboard.solve_after_recipes(9));
 
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard = Scoreboard::from(vec![3,7], (0,1));
         assert_eq!("0124515891", scoreboard.solve_after_recipes(5));
 
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard = Scoreboard::from(vec![3,7], (0,1));
         assert_eq!("9251071085", scoreboard.solve_after_recipes(18));
 
-        let mut scoreboard = Scoreboard { recipe_scores: vec![3,7], current_recipes: (0,1) };
+        let mut scoreboard = Scoreboard::from(vec![3,7], (0,1));
         assert_eq!("5941429882", scoreboard.solve_after_recipes(2018));
     }
 
